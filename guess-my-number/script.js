68f309feb7c0ad61dfc72a10c03d16707985da36
Diego Bezerra <diegobmartins21@gmail.com>
2025-05-04 10:09:32 -0300

    guessMyNumber app, applied guess and check score features

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,52 @@
 "use strict";
+
+// //select
+// console.log(document.querySelector(".message").textContent);
+
+// //manipulate value
+// document.querySelector(".message").textContent = "🎉 Correct Number!";
+// document.querySelector(".guess").value = 23;
+// // console.log(document.querySelector(".guess").value);
+
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".score").textContent = score;
+
+//Helpers
+
+const showMessage = function (element, message) {
+  element.textContent = message;
+};
+
+const scoreCounter = function () {
+  score--;
+  document.querySelector(".score").textContent = score;
+  return score;
+};
+
+const checkInputNumber = function (inputSelector, messageSelector) {
+  const guess = Number(document.querySelector(inputSelector).value);
+  const message = document.querySelector(messageSelector);
+  if (!guess) {
+    showMessage(message, "⛔ there's no number!");
+  } else if (guess < 1 || guess > 20) {
+    showMessage(message, "⛔ Insert a number between 1 and 20!");
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      showMessage(message, "🔼 More!");
+      scoreCounter();
+    } else showMessage(message, "😓 You LOSE!");
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      showMessage(message, "🔽 Less!");
+      scoreCounter();
+    } else showMessage(message, "😓 You LOSE!");
+  } else {
+    showMessage(message, "💚 Yeah, it's right! 🎉");
+    document.querySelector(".number").textContent = secretNumber;
+  }
+};
+
+document.querySelector(".check").addEventListener("click", () => {
+  checkInputNumber(".guess", ".message");
+});
